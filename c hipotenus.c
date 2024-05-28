e50e585d2d84188bdebe9b88964fb7cc9aa6aba2
zorbeyyavas <yavaszorbey@gmail.com>
2024-05-29 02:09:24 +0300

    Add files via upload

--- c hipotenus.c
+++ c hipotenus.c
@@ -1,3 +1,13 @@
 int main(){
-    
+    double a,b,c;
+
+printf("\n a kenarini giriniz");
+scanf("%lf",&a);
+
+printf("\n b kenarini giriniz");
+scanf("%lf",&b);
+
+c = sqrt(a*a + b*b );
+
+printf("\n c kenari uzunlugu = %.2lf",c);
 }
